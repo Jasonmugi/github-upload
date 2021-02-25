9c4995496ec965b781863f4464d48c779418e9a9
Jasonmugi <58854927+Jasonmugi@users.noreply.github.com>
2021-02-25 17:24:50 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,5 @@
+const msg = ['Awsome you are.','Upcoming Dev','Im the CEO','Jesus is good story','God is ready when you are ready.']
+
+const randMsg = msg[Math.floor(Math.random() *5)]
+
+console.log(randMsg)
