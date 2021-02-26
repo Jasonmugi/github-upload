6be74e9195b383980704a0b935b466eae262f52e
Jasonmugi <58854927+Jasonmugi@users.noreply.github.com>
2021-02-26 14:42:00 +0300

    update msg in app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const msg = ['Awsome you are.','Upcoming Dev','Im the CEO','Jesus is good story','God is ready when you are ready.']
+const msg = ['Awsome you are.','Upcoming Dev','Im the CEO','good story','ready when you are ready.']
 
 const randMsg = msg[Math.floor(Math.random() *5)]
 
